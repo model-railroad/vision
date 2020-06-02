f038e6cfec5b53ae8ac9e9e079047120bcc8ad6f
ralf <ralfoide@gmail.com>
2020-06-02 00:27:26 -0700

    Randall Experiment Cam Proxy: Jetty http serv serving image from camera.

--- java/src/main/java/com/alfray/camproxy/cam/HttpServ.java
+++ java/src/main/java/com/alfray/camproxy/cam/HttpServ.java
@@ -4,14 +4,20 @@ import com.alfray.camproxy.CommandLineArgs;
 import com.alfray.camproxy.util.DebugDisplay;
 import com.alfray.camproxy.util.ILogger;
 import com.alfray.camproxy.util.IStartStop;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,16 +26,22 @@ import java.util.regex.Pattern;
 public class HttpServ implements IStartStop {
     private static final String TAG = HttpServ.class.getSimpleName();
     private final ILogger mLogger;
+    private final Cameras mCameras;
     private final DebugDisplay mDebugDisplay;
     private final Server mServer;
+    private final Java2DFrameConverter mFrameConverter;
 
     @Inject
     public HttpServ(
             ILogger logger,
+            Cameras cameras,
             DebugDisplay debugDisplay,
             CommandLineArgs commandLineArgs) {
         mLogger = logger;
+        mCameras = cameras;
         mDebugDisplay = debugDisplay;
+
+        mFrameConverter = new Java2DFrameConverter(); // hangs when created in a thread
 
         int port = commandLineArgs.getIntOption(CommandLineArgs.OPT_HTTP_PORT, 8080);
         mServer = new Server(port);
@@ -62,7 +74,7 @@ public class HttpServ implements IStartStop {
             }
 
             if (!success) {
-                replyText(response, HttpServletResponse.SC_BAD_REQUEST, "Bad Request");
+                sendText(response, HttpServletResponse.SC_BAD_REQUEST, "Bad Request");
             }
             baseRequest.setHandled(true);
         }
@@ -71,24 +83,48 @@ public class HttpServ implements IStartStop {
 
         private boolean doGet(String path, HttpServletResponse response) throws IOException {
             if (path.equals("/qqq")) {
-                replyText(response, HttpServletResponse.SC_OK, "Quitting");
+                sendText(response, HttpServletResponse.SC_OK, "Quitting");
                 mDebugDisplay.requestQuit();
                 return true;
             }
 
             Matcher m = PATH_IMG_NUMBER.matcher(path);
             if (m.matches()) {
-                replyText(response, HttpServletResponse.SC_OK, "Image " + m.group(1));
-                return true;
+                try {
+                    CamInfo cam = mCameras.getByIndex(Integer.parseInt(m.group(1)));
+                    return sendImage(cam, response);
+                } catch (NumberFormatException e) {
+                    mLogger.log(TAG, "Invalid camera number: " + path);
+                }
             }
 
             return false;
         }
 
-        private void replyText(HttpServletResponse response, int code, String msg) throws IOException {
+        private void sendText(HttpServletResponse response, int code, String msg) throws IOException {
             response.setContentType("text/html; charset=utf-8");
             response.setStatus(code);
             response.getWriter().println(msg);
+        }
+
+        private boolean sendImage(CamInfo cam, HttpServletResponse response) throws IOException {
+            if (cam != null) {
+                Frame frame = cam.getGrabber().getLastFrame().get();
+                if (frame != null) {
+                    BufferedImage image  = mFrameConverter.convert(frame);
+                    if (image != null) {
+                        response.setContentType("image/jpeg");
+                        response.addHeader("Cache-Control", "no-store");
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        ServletOutputStream outputStream = response.getOutputStream();
+                        ImageIO.write(image, "jpg", outputStream);
+                        outputStream.flush();
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
