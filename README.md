bd57e1645caa5e2256ffbee85678c18c34dbaa59
ralf <ralfoide@gmail.com>
2021-07-24 12:40:22 -0700

    Train Motion: GPL v3 License.

--- README.md
+++ README.md
@@ -66,9 +66,25 @@ The web browser output is still available as an alternative.
 
 ## License
 
-__Train Motion__ is licensed under the terms of the open source [MIT License](https://opensource.org/licenses/MIT "MIT License").
+__Train Motion__ is licensed under the terms of the open source
+[GNU General Public License version 3](https://opensource.org/licenses/GPL-3.0 "GPL v3").
 
-The full text of the license is provided in the file `LICENSE.txt`.
+    Copyright (C) 2020-2021 alf.labs gmail com,
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The full GPL license is available in the file `LICENSE.txt`.
 
 
 ## Dependencies
--- src/main/java/com/alfray/trainmotion/cam/FakeFrameGrabber.java
+++ src/main/java/com/alfray/trainmotion/cam/FakeFrameGrabber.java
@@ -1,3 +1,21 @@
+/*
+ * Project: Train-Motion
+ * Copyright (C) 2021 alf.labs gmail com,
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.alfray.trainmotion.cam;
 
 import org.bytedeco.javacv.Frame;
@@ -22,8 +40,7 @@ public class FakeFrameGrabber implements IFrameGrabber {
     private static final int FRAME_RATE_FPS = 10;
     public static final String PREFIX = "fake_srgb_";
     private final int mSpeedRgb;
-    @Nonnull
-    private final String mInputUrl;
+    @Nonnull private final String mInputUrl;
     private OpenCVFrameConverter.ToMat mMatConverter;
     private Mat mMat;
     private int mX, mY;
