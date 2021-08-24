169a68d642b84d0604757192e0d5a44db1572b7f
ralf <ralfoide@gmail.com>
2021-08-24 00:33:40 -0700

    Train Motion: Update run script to list java version.

--- _build_and_run.sh
+++ _build_and_run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e
-set -x
 (
     # Change to this script direcoty (following symlinks as needed)
     cd $(dirname $(readlink "$BASH_SOURCE" || echo "$BASH_SOURCE"))
+
+    echo "JAVA_HOME=${JAVA_HOME}"
+    [[ -d "$JAVA_HOME" ]] && JV="$JAVA_HOME/bin/java.exe" || JV=$(which java)
+    V=$( "$JV" -version 2>&1 | sed -n -e '/build /s/.*build \([0-9]\+\.[0-9]\+\).*/\1/p' | head -n 1 )
+    echo "Java version: $V"
+
     # Build
-    ./gradlew test ass fatJar --info
+    set -x
+    ./gradlew test ass fatJar --console=plain --info
     # ./gradlew --stop
 )
 # List & run
 ls -1sh $(find build/ -name "*.jar")
+set -x
 java -jar build/libs/train-motion-0.2-SNAPSHOT-all.jar $@
