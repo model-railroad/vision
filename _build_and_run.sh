a33634838c9e6dd58c21f2edfe634df2a091136c
Ralf <ralfoide@gmail.com>
2021-08-29 12:09:15 -0700

    Train Motion: Gradle run with classpath on Linux.

--- _build_and_run.sh
+++ _build_and_run.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 set -e
-DST="build"
-if [[ ! -d "build" ]]; then
-    DST="train-motion/$DST"
-fi
-JAR="$DST/libs/train-motion-0.2-SNAPSHOT-all.jar"
 
-if [[ ! -x "$JAR" || "$USER" != "vision" ]]; then
-(
-    # Change to this script direcoty (following symlinks as needed)
-    cd $(dirname $(readlink "$BASH_SOURCE" || echo "$BASH_SOURCE"))
+C_DIR="$PWD"
+G_DIR=$(dirname $(readlink "$BASH_SOURCE" || echo "$BASH_SOURCE"))
 
-    echo "JAVA_HOME=${JAVA_HOME}"
-    [[ -d "$JAVA_HOME" ]] && JV="$JAVA_HOME/bin/java.exe" || JV=$(which java)
-    V=$( "$JV" -version 2>&1 | sed -n -e '/build /s/.*build \([0-9]\+\.[0-9]\+\).*/\1/p' | head -n 1 )
-    echo "Java version: $V"
+echo "JAVA_HOME=${JAVA_HOME}"
+[[ -d "$JAVA_HOME" ]] && JV="$JAVA_HOME/bin/java.exe" || JV=$(which java)
+V=$( "$JV" -version 2>&1 | sed -n -e '/build /s/.*build \([0-9]\+\.[0-9]\+\).*/\1/p' | head -n 1 )
+echo "Java version: $V"
 
-    # Build
-    set -x
-    ./gradlew test ass fatJar --console=plain --info
-    # ./gradlew --stop
-)
-fi
+CONFIG=""
+if [[ -f "$C_DIR/config.ini" ]]; then CONFIG="--config $C_DIR/config.ini"; fi
 
-# List & run
-ls -1sh $(find $DST/ -name "*.jar")
+# Build & invoke with command line pargs
 set -x
-java -jar $JAR $@
+cd $G_DIR && ./gradlew run -Pargs="$CONFIG $@" --console=plain
+
